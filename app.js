364cb492afa0f5d88fa00a96841c30e9cbfe6b3c
HiyokoChan0822 <moe.yoshida0822@gmail.com>
2024-06-09 22:43:04 +0900

    cps

--- app.js
+++ app.js
@@ -1,60 +1,177 @@
 import 'dotenv/config';
-import express from 'express';
+import { InteractionType, InteractionResponseType } from 'discord-interactions';
+import { Client } from 'discord.js';
+import { getResTest } from './test.js';
 import {
-  InteractionType,
-  InteractionResponseType,
-  InteractionResponseFlags,
-  MessageComponentTypes,
-  ButtonStyleTypes,
-} from 'discord-interactions';
-import { VerifyDiscordRequest, getRandomEmoji, DiscordRequest } from './utils.js';
-import { getShuffledOptions, getResult } from './game.js';
+  getResRecruiting,
+  getResRecruitingEntry,
+  getResRecruitingMatching,
+  getResRecruitingReset,
+} from './recruiting.js';
+import {
+  getResGacha,
+  getResGachaHero,
+  getResGachaCard,
+} from './gacha.js';
 
-// Create an express app
-const app = express();
-// Get port, or default to 3000
-const PORT = process.env.PORT || 3000;
-// Parse request body and verifies incoming requests using discord-interactions package
-app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
+let req = {};
 
-// Store for in-progress games. In production, you'd want to use a DB
-const activeGames = {};
+const client = new Client({ intents: [1] });
 
-/**
- * Interactions endpoint URL where Discord will send HTTP requests
+// TODO: app.post()で実装
+client.on('interactionCreate', async res => {
+  req = res;
+  const { type, data } = getInteraction();
+  try {
+    switch ( type ) {
+      case InteractionResponseType.PONG:
+        res.ping(data);
+        break;
+      case InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE:
+        res.reply(data);
+        break;
+      case InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE:
+        res.reply(data);
+        break;
+      case InteractionResponseType.DEFERRED_UPDATE_MESSAGE:
+        res.update(data);
+        break;
+      case InteractionResponseType.UPDATE_MESSAGE:
+        res.update(data);
+        break;
+      case InteractionResponseType.APPLICATION_COMMAND_AUTOCOMPLETE_RESULT:
+        res.update(data);
+        break;
+      case InteractionResponseType.MODAL:
+        res.showModal(data);
+        break;
+      default:
+        return;
+    };
+  } catch (err) {
+    console.error(err);
+  };
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+/** 
+ * Get response for interaction type
+ * @param {Option} data - Interaction data payload
+ * @return {Option}
+ * @see https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object-interaction-type
  */
-app.post('/interactions', async function (req, res) {
-  // Interaction type and data
-  const { type, id, data } = req.body;
+function getInteraction() {
+  const { type } = req;
 
-  /**
-   * Handle verification requests
-   */
-  if (type === InteractionType.PING) {
-    return res.send({ type: InteractionResponseType.PONG });
-  }
+  switch ( type ) {
+    case InteractionType.PING:
+      return getResPing();
+    case InteractionType.APPLICATION_COMMAND:
+      return getResAppricationCommand();
+    case InteractionType.MESSAGE_COMPONENT:
+      return getResMessegeComponent();
+    case InteractionType.APPLICATION_COMMAND_AUTOCOMPLETE:
+      return getResApplicationCommandAutocomplete();
+    case InteractionType.MODAL_SUBMIT:
+      return getResModalSubmit();
+    default:
+      return getResError('Interaction type error');
+  };
+};
 
-  /**
-   * Handle slash command requests
-   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-   */
-  if (type === InteractionType.APPLICATION_COMMAND) {
-    const { name } = data;
+/** 
+ * Get response for ping
+ * @return {Option}
+ */
+function getResPing(){
+  return { type: InteractionResponseType.PONG };
+};
 
-    // "test" command
-    if (name === 'test') {
-      // Send a message into the channel where command was triggered from
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          // Fetches a random emoji to send from a helper function
-          content: 'hello world ' + getRandomEmoji(),
-        },
-      });
-    }
-  }
-});
+/** 
+ * コマンド
+ * Get response for apprication command
+ * @return {Option}
+ * @see https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object-application-command-data-structure
+ */
+function getResAppricationCommand() {
+  const { commandName } = req;
 
-app.listen(PORT, () => {
-  console.log('Listening on port', PORT);
-});
+  switch ( commandName ) {
+    case 'test':
+      return getResTest();
+    case 'recruiting':
+      return getResRecruiting();
+    case 'gacha':
+      return getResGacha();
+    default:
+      return getResError('Apprication command error');
+  };
+};
+
+/** 
+ * ボタン・セレクトメニュー
+ * Get response for message component
+ * @return {Option}
+ * @see https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object-application-command-data-structure
+ */
+function getResMessegeComponent() {
+  const { customId } = req;
+
+  switch ( customId ) {
+    case 'recruiting-entry':
+      return getResRecruitingEntry();
+    case 'recruiting-matching':
+      return getResRecruitingMatching();
+    case 'recruiting-reset':
+      return getResRecruitingReset();
+    case 'gacha-heros':
+      return getResGachaHero();
+    case 'gacha-cards':
+      return getResGachaCard();
+    default:
+      return getResError('Message component response error');
+  };
+};
+
+/** 
+ * モーダル
+ * Get response for modal submit
+ * @return {Option}
+ * @see https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object-application-command-data-structure
+ */
+function getResModalSubmit() {
+  const { customId } = req;
+
+  switch ( customId ) {
+    default:
+      return getResError('Modal submit error');
+  };
+};
+
+/** 
+ * Get response for error
+ * @return {Option}
+ */
+export function getResError(str) {
+  return { 
+    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    data: { content: str },
+  };
+};
+
+export function getReqSubCommand() {
+  return req.options._subcommand;
+};
+
+export function getUserId() {
+  return req.member.user.id;
+};
+
+export function getReqEmbeds() {
+  return req.message.embeds;
+};
+
+export function getReqComponents() {
+  return req.message.components;
+};
--- commands.js
+++ commands.js
@@ -1,45 +1,191 @@
 import 'dotenv/config';
-import { getRPSChoices } from './game.js';
-import { capitalize, InstallGlobalCommands } from './utils.js';
+import { InstallGlobalCommands } from './utils.js';
 
-// Get the game choices from game.js
-function createCommandChoices() {
-  const choices = getRPSChoices();
-  const commandChoices = [];
-
-  for (let choice of choices) {
-    commandChoices.push({
-      name: capitalize(choice),
-      value: choice.toLowerCase(),
-    });
-  }
-
-  return commandChoices;
-}
-
-// Simple test command
+/**
+ * @type {Object} Test command
+ */
 const TEST_COMMAND = {
   name: 'test',
   description: 'Basic command',
+  default_member_permission: false,
   type: 1,
 };
 
-// Command containing options
-const CHALLENGE_COMMAND = {
-  name: 'challenge',
-  description: 'Challenge to a match of rock paper scissors',
+/**
+ * @type {Object} Bio command
+ */
+const BIO_COMMAND = {
+  name: 'bio',
+  description: 'Bio command',
+  default_member_permission: false,
   options: [
     {
-      type: 3,
-      name: 'object',
-      description: 'Pick your object',
-      required: true,
-      choices: createCommandChoices(),
+      name: 'button',
+      description: 'Set bio buttons',
+      default_member_permission: false,
+      type: 1,
     },
   ],
   type: 1,
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+/**
+ * @type {Object} Recruiting command
+ */
+const RECRUITING_COMMAND = {
+  name: 'recruiting',
+  description: 'Recruiting member',
+  default_member_permission: true,
+  options: [
+    {
+      name: 'custom',
+      description: 'Recruiting member for custom battle',
+      type: 1,
+    },
+  ],
+  type: 1,
+};
+
+/**
+ * @type {Object} Gacha command
+ */
+const GACHA_COMMAND = {
+  name: 'gacha',
+  description: 'Get hero or card gacha results',
+  default_member_permission: true,
+  options: [
+    {
+      name: 'heros',
+      description: 'Get hero gacha results',
+      options: [
+        {
+          name: 'attacker',
+          description: 'Include attacker hero? (default: true)',
+          type: 5,
+          required: false,
+        },
+        {
+          name: 'gunner',
+          description: 'Include gunner hero? (default: true)',
+          type: 5,
+          required: false,
+        },
+        {
+          name: 'tank',
+          description: 'Include tank hero? (default: true)',
+          type: 5,
+          required: false,
+        },
+        {
+          name: 'sprinter',
+          description: 'Include sprinter hero? (default: true)',
+          type: 5,
+          required: false,
+        },
+      ],
+      type: 1,
+    },
+    {
+      name: 'cards',
+      description: 'Get card gacha results',
+      options: [
+        {
+          name: 'ur',
+          description: 'include UR card? (default: true)',
+          type: 5,
+          required: false,
+        },
+        {
+          name: 'sr',
+          description: 'include SR card? (default: false)',
+          type: 5,
+          required: false,
+        },
+        {
+          name: 'r',
+          description: 'include R card? (default: false)',
+          type: 5,
+          required: false,
+        },
+        {
+          name: 'n',
+          description: 'include N card? (default: false)',
+          type: 5,
+          required: false,
+        },
+      ],
+      type: 1,
+    },
+  ],
+};
+
+/**
+ * @type {Object} Quiz command
+ */
+const QUIZ_COMMAND = {
+  name: 'quiz',
+  description: 'Quiz command',
+  default_member_permission: false,
+  type: 1,
+};
+
+/**
+ * @type {Object} Score command
+ */
+const SCORE_COMMAND = {
+  name: 'score',
+  description: 'Score command',
+  default_member_permission: false,
+  type: 1,
+};
+
+/**
+ * @type {Object} Generator command
+ */
+const GENERATOR_COMMAND = {
+  name: 'generator',
+  description: 'Generator command',
+  default_member_permission: false,
+  type: 1,
+};
+
+/**
+ * @type {Object} Point command
+ */
+const POINT_COMMAND = {
+  name: 'point',
+  description: 'Point command',
+  default_member_permission: false,
+  type: 1,
+};
+
+/**
+ * @type {Object} Contact command
+ */
+const CONTACT_COMMAND = {
+  name: 'contact',
+  description: 'Contact command',
+  default_member_permission: false,
+  type: 1,
+};
+
+/**
+ * @type {Object} Help command
+ */
+const HELP_COMMAND = {
+  name: 'help',
+  description: 'Help command',
+  default_member_permission: false,
+  type: 1,
+};
+
+/**
+ * @type {Object} All commands
+ */
+const ALL_COMMANDS = [
+  TEST_COMMAND,
+  RECRUITING_COMMAND,
+  GACHA_COMMAND,
+];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
